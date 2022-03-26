7e93a97c4819b13688a43af2e88c3c35f8ba696e
og1842 <og1842@gmail.com>
2022-03-26 17:09:31 +0400

    fb google login test

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
---
+++ src/Controller/TermsController.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TermsController extends AbstractController
+{
+    #[Route('/terms', name: 'app_terms')]
+    public function index(): Response
+    {
+        return $this->render('terms/index.html.twig');
+    }
+
+}
