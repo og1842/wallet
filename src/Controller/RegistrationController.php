3f15d9b1ad1749e9102e19314dee2ac31333f9cd
og1842 <og1842@gmail.com>
2022-03-27 01:08:14 +0400

    fb google login test

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('mailer@main-bvxea6i-sd2upeicfcany.de-2.platformsh.site', 'Bot'))
+                    ->from(new Address('mailer@main-bvxea6i-sd2upeicfcany.de-2.platformsh.site', 'Wallet system'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -36,6 +36,7 @@ class EmailVerifier
         $context['signedUrl'] = $signatureComponents->getSignedUrl();
         $context['expiresAtMessageKey'] = $signatureComponents->getExpirationMessageKey();
         $context['expiresAtMessageData'] = $signatureComponents->getExpirationMessageData();
+        $context['firstName'] = $user->getFirstName();
 
         $email->context($context);
 
