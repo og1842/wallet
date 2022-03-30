64459176c024bf741dc74e90549c83d9f2055879
og1842 <og1842@gmail.com>
2022-03-30 12:08:47 +0400

    unit tests

--- tests/Service/WalletServiceTest.php
+++ tests/Service/WalletServiceTest.php
@@ -36,4 +36,66 @@ class WalletServiceTest extends KernelTestCase
 
         $this->assertTrue($status, 'Is waller created');
     }
+
+    public function testFillUserBalance(): void
+    {
+        // (1) boot the Symfony kernel
+        self::bootKernel([
+            'debug' => false
+        ]);
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        // (3) run some service & test the result
+        $entityManager = $container->get(EntityManagerInterface::class);
+
+        $userRepository = $entityManager->getRepository(User::class);
+
+        $user = $userRepository->findOneBy(['email' => 'test@test.test']);
+
+        if (!$user instanceof User) {
+            $this->fail('User not found. Please run bin/console --env=test doctrine:fixtures:load');
+        }
+
+        $walletService = $container->get(WalletService::class);
+        $status = $walletService->fillUserBalance($user->getId(), '88645711a0134467bcc1a3f6797b2340', 10408, 'Fill balance');
+
+        $this->assertTrue($status, 'User balance is filled');
+
+        $status = $walletService->fillUserBalance($user->getId(), '1111', 10408, 'Fill balance');
+
+        $this->assertFalse($status, 'User balance is not filled');
+    }
+
+    public function testTransfer(): void
+    {
+        // (1) boot the Symfony kernel
+        self::bootKernel([
+            'debug' => false
+        ]);
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        // (3) run some service & test the result
+        $entityManager = $container->get(EntityManagerInterface::class);
+
+        $userRepository = $entityManager->getRepository(User::class);
+
+        $user = $userRepository->findOneBy(['email' => 'test@test.test']);
+
+        if (!$user instanceof User) {
+            $this->fail('User not found. Please run bin/console --env=test doctrine:fixtures:load');
+        }
+
+        $walletService = $container->get(WalletService::class);
+        $status = $walletService->transfer($user->getId(), '88645711a0134467bcc1a3f6797b2340', 'a41225c57f0e4a82abbc3a0e995a7c5f', '10408', 'Transfer');
+
+        $this->assertTrue($status, 'Transfer done');
+
+        $status = $walletService->transfer($user->getId(), '88645711a0134467bcc1a3f6797b2340', '88645711a0134467bcc1a3f6797b2340', '10408', 'Transfer');
+
+        $this->assertFalse($status, 'Transfer failed');
+    }
 }
--- tests/Service/WalletValidatorTest.php
+++ tests/Service/WalletValidatorTest.php
@@ -60,4 +60,106 @@ class WalletValidatorTest extends KernelTestCase
 
         $this->assertFalse($status, 'Wallet params for creation is not valid');
     }
+
+    public function testValidateRequestParamsForFillBalance(): void
+    {
+        // (1) boot the Symfony kernel
+        self::bootKernel([
+            'debug' => false
+        ]);
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        // (3) run some service & test the result
+        $entityManager = $container->get(EntityManagerInterface::class);
+
+        $userRepository = $entityManager->getRepository(User::class);
+
+        $user = $userRepository->findOneBy(['email' => 'test@test.test']);
+
+        if (!$user instanceof User) {
+            $this->fail('User not found. Please run bin/console --env=test doctrine:fixtures:load');
+        }
+
+        $walletValidator = $container->get(WalletValidator::class);
+
+        try {
+            $walletValidator->validateRequestParamsForFillBalance('88645711a0134467bcc1a3f6797b2340', 'credit_card', '100.55');
+            $status = true;
+        } catch (RuntimeException $ex) {
+            $status = false;
+        }
+
+        $this->assertTrue($status, 'Fill balance params is valid');
+
+        try {
+            $walletValidator->validateRequestParamsForFillBalance('88645711a0134467bcc1a3f6797b2340', 'cash', '115');
+            $status = true;
+        } catch (RuntimeException $ex) {
+            $status = false;
+        }
+
+        $this->assertTrue($status, 'Fill balance params is valid');
+
+        try {
+            $walletValidator->validateRequestParamsForFillBalance('88645711a0134467bcc1a3f6797b2340', '', '');
+            $status = true;
+        } catch (RuntimeException $ex) {
+            $status = false;
+        }
+
+        $this->assertFalse($status, 'Fill balance params is not valid');
+    }
+
+    public function testValidateRequestParamsForTransfer(): void
+    {
+        // (1) boot the Symfony kernel
+        self::bootKernel([
+            'debug' => false
+        ]);
+
+        // (2) use static::getContainer() to access the service container
+        $container = static::getContainer();
+
+        // (3) run some service & test the result
+        $entityManager = $container->get(EntityManagerInterface::class);
+
+        $userRepository = $entityManager->getRepository(User::class);
+
+        $user = $userRepository->findOneBy(['email' => 'test@test.test']);
+
+        if (!$user instanceof User) {
+            $this->fail('User not found. Please run bin/console --env=test doctrine:fixtures:load');
+        }
+
+        $walletValidator = $container->get(WalletValidator::class);
+
+        try {
+            $walletValidator->validateRequestParamsForTransfer('88645711a0134467bcc1a3f6797b2340', 'a41225c57f0e4a82abbc3a0e995a7c5f','Transfer', '100.55');
+            $status = true;
+        } catch (RuntimeException $ex) {
+            $status = false;
+        }
+
+        $this->assertTrue($status, 'Transfer params is valid');
+
+        try {
+            $walletValidator->validateRequestParamsForTransfer('88645711a0134467bcc1a3f6797b2340', 'a41225c57f0e4a82abbc3a0e995a7c5f','Transfer', '100.55');
+            $status = true;
+        } catch (RuntimeException $ex) {
+            $status = false;
+        }
+
+        $this->assertTrue($status, 'Transfer params is valid');
+
+        try {
+            $walletValidator->validateRequestParamsForTransfer('88645711a0134467bcc1a3f6797b2340', '88645711a0134467bcc1a3f6797b2340','Transfer', '100.55');
+            $status = true;
+        } catch (RuntimeException $ex) {
+            $status = false;
+        }
+
+        $this->assertFalse($status, 'Transfer params is not valid');
+    }
 }
